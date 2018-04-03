325bcdec9a12ca7447bb2b3ad24e37f693523383
Julio Sánchez <30514115+julioEduardo@users.noreply.github.com>
2018-04-03 16:10:28 -0500

    terminado

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var flag = false
+var valor_de_display = document.getElementById("display")
 function animar(elemento){
 	elemento.style.transform="scale(0.9)";
 	setTimeout(function () {
@@ -6,82 +6,129 @@ function animar(elemento){
 	}, 100);
 }
 function display(elemento){
-	var valor_de_display = document.getElementById("display")
-	if(!isNaN(elemento.id)){
-		if(valor_de_display.textContent==0 && !flag){
-			valor_de_display.textContent = elemento.id
-		}else if(valor_de_display.textContent.length<8){
-			valor_de_display.textContent = valor_de_display.textContent + elemento.id
-		}
+	if(valor_de_display.textContent==0 && valor_de_display.textContent.indexOf(".")==-1){
+		valor_de_display.textContent = elemento.id
+	}else{
+		valor_de_display.textContent = valor_de_display.textContent + elemento.id
+		verificarDisplay(valor_de_display.textContent)
 	}
 }
-function borrarDisplay(){
-	document.getElementById("display").textContent = 0
-	flag = false
-}
-function punto(){
-	if(!flag){
-		var valor_de_display = document.getElementById("display")
-		valor_de_display.textContent = valor_de_display.textContent + "."
-		flag = true
+function verificarDisplay(elemento){
+	if(elemento.length>8){
+		valor_de_display.textContent = elemento.substring(0, 8)
 	}
 }
-function funcionMasMenos(){
-	document.getElementById("display").textContent *= -1
+function funciones(elemento){
+	switch(elemento.id){
+		case "on":
+			Calculadora.on()
+			break;
+		case "punto":
+			Calculadora.punto()
+			break;
+		case "sign":
+			Calculadora.signo()
+			break;
+		case "mas":
+			Calculadora.sumar()
+			break;
+		case "menos":
+			Calculadora.restar()
+			break;
+		case "por":
+			Calculadora.multiplicar()
+			break;
+		case "dividido":
+			Calculadora.dividir()
+			break;
+		case "igual":
+			Calculadora.resolver()
+			break;
+	}
 }
 
 var Calculadora = {
 	init: function(){
-		this.asignarTeclas("tecla")
-		this.asignarFuncionON()
-		this.asignarTeclaPunto()
-		this.asignarMasMenos()
+		this.asignarTeclasNumericas("tecla")
+		this.asignarTeclasFunciones("tecla")
 	},
 	valor_a: 0,
 	valor_b: 0,
+	operacion: "",
 	resultado: 0,
-	asignarTeclas: function(selector){
-		var teclas = document.getElementsByClassName(selector)
-		for(i = 0; i < teclas.length; i++){
-			teclas[i].onclick = this.eventoPresionarTecla
+	asignarTeclasNumericas: function(selector){
+		var teclas_de_numeros = document.getElementsByClassName(selector)
+		for(i = 0; i < teclas_de_numeros.length; i++){
+			if(!isNaN(teclas_de_numeros[i].id))
+			teclas_de_numeros[i].onclick = this.eventoClickTeclaNumerica
 		}
 	},
-	eventoPresionarTecla: function(event){
+	asignarTeclasFunciones(selector){
+		var teclas_de_funcion = document.getElementsByClassName(selector)
+		for(i in teclas_de_funcion){
+			if(isNaN(teclas_de_funcion[i].id)){
+				teclas_de_funcion[i].onclick = this.eventoClickTeclaFuncion
+			}
+		}
+	},
+	eventoClickTeclaFuncion: function(event){
+		animar(event.target)
+		funciones(event.target)
+	},
+	eventoClickTeclaNumerica: function(event){
 		animar(event.target)
 		display(event.target)
 	},
-	asignarFuncionON: function(){
-		document.getElementById("on").onclick = this.eventoBorrarDisplay
+	on: function(){
+		valor_de_display.textContent = 0
 	},
-	eventoBorrarDisplay: function(event){
-		animar(event.target)
-		borrarDisplay()
+	punto: function(){
+		if(valor_de_display.textContent.indexOf(".")==-1){
+			valor_de_display.textContent = valor_de_display.textContent + "."
+		}
 	},
-	asignarTeclaPunto: function(){
-		document.getElementById("punto").onclick = this.eventoTeclaPunto
+	signo: function(){
+		valor_de_display.textContent *= -1
 	},
-	eventoTeclaPunto: function(event){
-		animar(event.target)
-		punto()
-	},
-	asignarMasMenos: function (){
-		document.getElementById("sign").onclick = this.eventoTeclaMasMenos
-	},
-	eventoTeclaMasMenos: function(event){
-		animar(event.target)
-		funcionMasMenos()
-	},
-	sumar: function(){
 
+	sumar: function(){
+		valor_a = valor_de_display.textContent
+		operacion = "+"
+		valor_de_display.textContent = ""
 	},
 	restar: function(){
-
+		valor_a = valor_de_display.textContent
+		operacion = "-"
+		valor_de_display.textContent = ""
 	},
 	multiplicar: function(){
-
+		valor_a = valor_de_display.textContent
+		operacion = "*"
+		valor_de_display.textContent = ""
 	},
 	dividir: function(){
-
+		valor_a = valor_de_display.textContent
+		operacion = "/"
+		valor_de_display.textContent = ""
+	},
+	resolver: function(){
+		valor_b = valor_de_display.textContent
+		switch(operacion){
+			case "+":
+				resultado = parseFloat(valor_a) + parseFloat(valor_b)
+				break;
+			case "-":
+				resultado = parseFloat(valor_a) - parseFloat(valor_b)
+				break;
+			case "*":
+				resultado = parseFloat(valor_a) * parseFloat(valor_b)
+				break;
+			case "/":
+				resultado = parseFloat(valor_a) / parseFloat(valor_b)
+				break;
+		}
+		valor_de_display.textContent = resultado
+		verificarDisplay(valor_de_display.textContent)
 	}
 }
 Calculadora.init();
